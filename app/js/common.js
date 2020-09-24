cbabd8e42d2721944f2a7626b888b25abee247d2
igorgo-hyuna <igor.gologots@hyuna.bb>
2020-09-24 18:23:19 +0300

    Added header section

--- app/js/common.js
+++ app/js/common.js
@@ -8,19 +8,30 @@ $(document).ready(function() {
         $(".top_mnu").fadeOut(600);
         $(".sandwich").toggleClass("active");
         $(".top_text").css("opacity", "1");
-    }).append("<span>");
+    });
 
     /* Burger. Open and close */
     $(".toggle_mnu").click(function() {
         if ($(".top_mnu").is(":visible")) {
             $(".top_text").css("opacity", "1");
             $(".top_mnu").fadeOut(600);
-            $(".top_mnu li a").removeClass("fadeInUp animated");
+            $(".top_mnu li a").removeClass("fadeIn animated");
         } else {
             $(".top_text").css("opacity", ".1");
             $(".top_mnu").fadeIn(600);
-            $(".top_mnu li a").addClass("fadeInUp animated");
+            $(".top_mnu li a").addClass("fadeIn animated");
         };
+    });
+
+    /********** Search panel **********/
+    $(".modal-search").click(function() {
+        $(".modal-window-search").css("display", "block");
+        $(".modal-window-search").css("visibility", "visible");
+        $(".modal-window-search").addClass("fadeIn animated");
+    });
+    $(".toggle_mnu_search").click(function() {
+        $(".modal-window-search").css("display", "none");
+        $(".modal-window-search").css("visibility", "hidden");
     });
 
     /* Parallax 
