5993635354677861c9ddfd43e10bf8aef9456605
igorgo-hyuna <igor.gologots@hyuna.bb>
2020-09-29 19:22:09 +0300

    Added all changes

--- app/js/common.js
+++ app/js/common.js
@@ -40,6 +40,9 @@ $(document).ready(function() {
         loop: true,
         nav: true,
         margin: 10,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
         responsive: {
             0: {
                 items: 1
